ff78a84ebea950382bc0278a1a8793fb093f352a
Javi Márquez <57186876+jmarquezs2@users.noreply.github.com>
2020-05-15 12:17:06 +0200

    Add files via upload

--- src/MathNotes/src/org/jmarquezs/main/Main.java
+++ src/MathNotes/src/org/jmarquezs/main/Main.java
@@ -14,16 +14,23 @@ public class Main {
 		UsuarioDAOimpl.register("juan@gmail.com", "juan12", "Juan");
 		UsuarioDAOimpl.changePassword("juan@gmail.com", "juan123",  "juan123");
 		System.out.println(UsuarioDAOimpl.readRol("pepito@gmail.com"));
+		
+		
 		Content cont = NoteDAOimpl.createContent("x=2*3", "formula");
 		Content cont2 = NoteDAOimpl.createContent("https://www.filmaffinity.com/es/main.html", "link");
 		NoteDAOimpl.createNote(1, "Prueba", "Fisica", "Leyes", "hola hola", cont, cont2, "pepito@gmail.com");
+		
 		Content cont3 = NoteDAOimpl.createContent("V=ab*h", "formula");
 		NoteDAOimpl.createNote(1, "Volumen", "Matematicas", "Volumenes", "hola hola", cont3, null, "pepito@gmail.com");
+		
 		Content cont4 = NoteDAOimpl.createContent("g=9,8", "formula");
 		NoteDAOimpl.createNote(1, "Gravedad", "Fisica", "Fuerzas", "hola hola", cont4, null, "pepito@gmail.com");
 		
 		//Content cont5 = NoteDAOimpl.createContent("v=e/t", "formula");
-		//NoteDAOimpl.createNote(1, "Velocidad", "Fisica", "Movimiento", "La velocidad", cont5, null, "juan@gmail.com");
+		//NoteDAOimpl.createNote(1, "Velocidad", "Fisica", "Movimiento", "La velocidad", cont5, null, "pepito@gmail.com");
+		
+		
+		
 	}
 
 }
